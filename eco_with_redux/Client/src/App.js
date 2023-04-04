e53071a8b6bfc61dedd0566324f0d09ba0921e68
ehsan-0801 <ehsanulahmmed@gmail.com>
2023-04-04 07:18:28 +0600

    ecommerce with context_api and useReducer has been practised and completed

---
+++ eco_with_redux/Client/src/App.js
@@ -0,0 +1,17 @@
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
+import ProductProvider from "./Context/ProductProvider";
+
+function App() {
+
+
+  return (
+    <div>
+      <ProductProvider>
+        <RouterProvider router={ routes } />
+      </ProductProvider>
+    </div>
+  );
+}
+
+export default App;
